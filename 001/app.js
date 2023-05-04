edc7efe44a05d088cafd120e702f01d2f71548da
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2023-05-04 12:53:00 +0300

    Initial commit

---
+++ 001/app.js
@@ -0,0 +1,24 @@
+console.log('Labukas, Zuiki');
+
+let a = 5; // number
+let b = '5'; // string
+let c = true; // boolean true => 1, false = 0
+
+a = a + c;
+b = b + c;
+
+// console.log('-->', a, typeof a);
+// console.log('-->', b, typeof b);
+
+
+let d;
+console.log('-->', d, typeof d);
+
+let e = null;
+console.log('-->', e, typeof e);
+
+let f = 'f';
+console.log('-->', ++f, typeof f);
+
+let z = 100 / 0;
+console.log('-->', z, typeof z);
