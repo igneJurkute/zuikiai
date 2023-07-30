797cdfb15d29f9dfb6ceeb0b2b2de8e379bbc750
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2023-07-30 21:40:17 +0300

    laravel colors

---
+++ miskas/app/Http/Controllers/ColorController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Color;
+use Illuminate\Http\Request;
+
+
+class ColorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $colors = Color::all();
+
+        return view('colors.index', [
+            'colors' => $colors
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('colors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $color = new Color;
+        $color->color = $request->color;
+        $color->author = $request->color_author ?? 'Anonymous';
+        $color->rate = $request->color_rate;
+        $color->save();
+        return redirect()->route('colors-index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Color $color)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Color $color)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Color $color)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Color $color)
+    {
+        //
+    }
+}
--- miskas/routes/web.php
+++ miskas/routes/web.php
@@ -1,9 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController as A;
 use App\Http\Controllers\CalculatorController as C;
-
+use App\Http\Controllers\ColorController as R;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,20 +13,20 @@ use App\Http\Controllers\CalculatorController as C;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 // Route::get('/animals', ['App\Http\Controllers\AnimalController', 'index']);
 Route::get('/animals', [A::class, 'animals']);
-
 Route::get('/animals/racoon/{color?}', [A::class, 'racoon']);
-
 Route::get('/calculator', [C::class, 'showCalculator'])->name('calculator');
 Route::post('/calculator', [C::class, 'doCalculator'])->name('do-calculator');
-
+Route::prefix('colors')->name('colors-')->group(function () {
+    Route::get('/', [R::class, 'index'])->name('index'); // GET /colors from URL:  colors Name: colors-index
+    Route::get('/create', [R::class, 'create'])->name('create'); // GET /colors/create from URL:  colors/create Name: colors-create
+    Route::post('/', [R::class, 'store'])->name('store'); // POST /colors from URL:  colors Name: colors-store
+});
 
 
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
